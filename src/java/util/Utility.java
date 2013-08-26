dd6ae807ed0bb7e2fa7f36792ca291590bc178da
Umberto <umbertopalo@hotmail.it>
2013-08-26 14:13:50 +0200

    aggiunta servlet implementato il primo punto, ovvero il case insensitive del nome dei parametri passati nel get di http

--- src/java/util/Utility.java
+++ src/java/util/Utility.java
@@ -5,7 +5,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package request;
+package util;
 
 import java.io.IOException;
 import java.io.PrintWriter;
---
+++ src/java/wfs/GetCapabilities.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wfs;
+
+/**
+ *
+ * @author Umberto
+ */
+public class GetCapabilities {
+    
+}
