79e25b906cf358a0fb68874a044ce36372a4635c
Umberto <umbertopalo@hotmail.it>
2013-09-24 21:22:38 +0200

    ho appena inserito nella getCapabilitiesResponse la FeatureTypeList, si deve mettere ora la FilterCapabilities

--- src/java/response/GetCapabilitiesResponse.java
+++ src/java/response/GetCapabilitiesResponse.java
@@ -226,20 +226,16 @@ public class GetCapabilitiesResponse {
         List<DCP> dcpList = new ArrayList<DCP>();
         DCP dcp1 = new DCP();
         HTTP http1 = new HTTP();
-
         QName namespaceget1 = new QName("http://www.w3.org/1999/xlink", "GET");
         QName namespacepost1 = new QName("http://www.w3.org/1999/xlink", "POST");
         RequestMethodType get1 = new RequestMethodType();
         get1.setTitle("GET");
         get1.setHref("http://localhost:8080/WFS_umb_fab/Servlet_wfs?");
         JAXBElement<RequestMethodType> getOrPostget1 = new JAXBElement<RequestMethodType>(namespaceget1, RequestMethodType.class, null, get1);
-        
         RequestMethodType post1 = new RequestMethodType();
         post1.setTitle("POST");
         post1.setHref("http://localhost:8080/WFS_umb_fab/Servlet_wfs");
         JAXBElement<RequestMethodType> getOrPostpost1 = new JAXBElement<RequestMethodType>(namespacepost1, RequestMethodType.class, null, post1);
-       
-        
         List<JAXBElement<RequestMethodType>> listGetOrSet = new ArrayList<JAXBElement<RequestMethodType>>();
         listGetOrSet.add(getOrPostget1);
         listGetOrSet.add(getOrPostpost1);
@@ -275,7 +271,6 @@ public class GetCapabilitiesResponse {
         domainType3.setValue(sectionList);
         parameterList.add(domainType3);
         operation1.setParameter(parameterList);
-
         //operazione describe feature type
         Operation operation2 = new Operation();
         operation2.setName("DescribeFeatureType");
@@ -283,20 +278,16 @@ public class GetCapabilitiesResponse {
         List<DCP> dcpList2 = new ArrayList<DCP>();
         DCP dcp2 = new DCP();
         HTTP http2 = new HTTP();
-
         QName namespaceget2 = new QName("http://www.w3.org/1999/xlink", "GET");
         RequestMethodType get2 = new RequestMethodType();
         get2.setTitle("GET");
         get2.setHref("http://localhost:8080/WFS_umb_fab/Servlet_wfs?");
-        JAXBElement<RequestMethodType> getOrPostget2 = new JAXBElement<RequestMethodType>(namespaceget2, RequestMethodType.class, null, get2);
-        
+        JAXBElement<RequestMethodType> getOrPostget2 = new JAXBElement<RequestMethodType>(namespaceget2, RequestMethodType.class, null, get2);       
         QName namespacepost2 = new QName("http://www.w3.org/1999/xlink", "POST");
         RequestMethodType post2 = new RequestMethodType();
         post2.setTitle("POST");
         post2.setHref("http://localhost:8080/WFS_umb_fab/Servlet_wfs");
         JAXBElement<RequestMethodType> getOrPostpost2 = new JAXBElement<RequestMethodType>(namespacepost2, RequestMethodType.class, null, post2);
-        
-        
         List<JAXBElement<RequestMethodType>> listGetOrSet2 = new ArrayList<JAXBElement<RequestMethodType>>();
         listGetOrSet2.add(getOrPostget2);
         listGetOrSet2.add(getOrPostpost2);
@@ -313,28 +304,23 @@ public class GetCapabilitiesResponse {
         domainType4.setValue(outputList2);
         parameterList2.add(domainType4);
         operation2.setParameter(parameterList2);
-
-
-        //operazione Get Feature
+         //operazione Get Feature
         Operation operation3 = new Operation();
         operation3.setName("GetFeature");
         operationList.add(operation3);
         List<DCP> dcpList3 = new ArrayList<DCP>();
         DCP dcp3 = new DCP();
         HTTP http3 = new HTTP();
-
         QName namespaceget3 = new QName("http://www.w3.org/1999/xlink", "GET");
         RequestMethodType get3 = new RequestMethodType();
         get3.setTitle("GET");
         get3.setHref("http://localhost:8080/WFS_umb_fab/Servlet_wfs?");
         JAXBElement<RequestMethodType> getOrPostget3 = new JAXBElement<RequestMethodType>(namespaceget3, RequestMethodType.class, null, get3);
-        
         QName namespacepost3 = new QName("http://www.w3.org/1999/xlink", "GET");
         RequestMethodType post3 = new RequestMethodType();
         post3.setTitle("POST");
         post3.setHref("http://localhost:8080/WFS_umb_fab/Servlet_wfs");
         JAXBElement<RequestMethodType> getOrPostpost3 = new JAXBElement<RequestMethodType>(namespacepost3, RequestMethodType.class, null, post3);
-        
         List<JAXBElement<RequestMethodType>> listGetOrSet3 = new ArrayList<JAXBElement<RequestMethodType>>();
         listGetOrSet3.add(getOrPostget3);
         listGetOrSet3.add(getOrPostpost3);
@@ -346,7 +332,6 @@ public class GetCapabilitiesResponse {
         List<DomainType> parameterList3 = new ArrayList<DomainType>();
         DomainType domainType5 = new DomainType();
         domainType5.setName("resultType");
-
         DomainType domainType6 = new DomainType();
         domainType6.setName("outputFormat");
         List<String> outputList3 = new ArrayList<String>();
@@ -359,9 +344,7 @@ public class GetCapabilitiesResponse {
         parameterList3.add(domainType5);
         parameterList3.add(domainType6);
         operation3.setParameter(parameterList3);
-
-
-        //setto le Operation
+//setto le Operation
         OM.setOperation(operationList);
         getCapabilitieResp.setOperationsMetadata(OM);
     }
